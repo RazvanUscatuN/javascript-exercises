fdd9a26bff720ce526f59ef213dd5440b74c8e9e
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-15 13:07:26 +0200

    Add my solution for problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let stringReverse = "";
+    let lastLetter;
+
+    if (stringLength < 1) return string;
+
+    // It reverse the string taking the last letter and adding to a new string
+    for(let i = 0; i < string.length; i++){
+        lastLetter = string.substr(stringLength -1, 1)
+        stringReverse += lastLetter;
+        stringLength--;
+    }
+    return stringReverse;
 
 };
 
