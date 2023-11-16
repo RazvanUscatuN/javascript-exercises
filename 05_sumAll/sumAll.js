8d20183e1b2b4bd7e791e76877ff70021d7116da
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-16 11:50:38 +0200

    add my solution to prob.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNumber, lastNumber) {
+    //sum all numbers between start and last(inclusive)
+    if(startNumber < 0 || lastNumber < 0) return "ERROR";
+    else if(typeof(startNumber) != "number" || typeof(lastNumber) != "number"){
+         return "ERROR";
+    }
 
+    let total = 0;
+    //it works even if start number is bigger
+    if(startNumber > lastNumber){
+        for(let i = lastNumber; i <= startNumber; i++){
+            total += i;
+        }
+    } else if (startNumber < lastNumber){
+        for(let i = startNumber; i <= lastNumber; i++){
+            total += i;
+        }
+    }
+
+    return total;
 };
 
 // Do not edit below this line
