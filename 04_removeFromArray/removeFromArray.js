209f15f28938d492b67fc5fe060d5b97c1e277fd
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-16 11:15:01 +0200

    add my solution to prob. 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...eliminatedElements) {
 
+    // it compare the each element from eliminatedElements with the main array
+    for(let i = 0; i< eliminatedElements.length; i++){
+       //remove the common element
+        if(mainArray.indexOf(eliminatedElements[i]) > -1){
+            mainArray.splice(mainArray.indexOf(eliminatedElements[i]), 1);
+        }
+    }
+    
+    return mainArray;
 };
 
 // Do not edit below this line
