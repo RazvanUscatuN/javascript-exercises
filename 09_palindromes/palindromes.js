34691c61392f149e34a019591c23a686c15b279a
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-12-05 16:39:37 +0200

    add my solution to prob 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (stringPalindromes) {
+  let result = "";
+  result = stringPalindromes.toLowerCase();
+  //eliminate all the white space
+  result = result.replaceAll(" ", "");
 
+  //eliminate all the punctuation
+  result = result.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+
+  //make the words backwards
+  let backwards = result.split("").reverse().join("");
+
+  //compare the words, if same return true else false
+  if (result == backwards) return true;
+  else return false;
 };
 
 // Do not edit below this line
