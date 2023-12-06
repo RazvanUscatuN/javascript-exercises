4d6a309e9b7c4e5b36cdb059d8e4e72f25b4a4f7
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-12-06 13:54:39 +0200

    add my solution to prob.11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+   return array.map(function(element){
+    return element["title"];
+   });
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
