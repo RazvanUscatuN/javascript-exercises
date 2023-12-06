c8ab39e8f322e41b8cf1fa704848412ba900e443
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-12-06 11:59:28 +0200

    add my solution to prob.10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  number = parseInt(number);
 
+  if (number < 0) return "OOPS";
+
+  let currentNumber = 0;
+  let increment = 0;
+  let placeHolder = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i == 0) {
+      currentNumber++;
+    } else if (i == 1) {
+      increment++;
+    } else {
+      placeHolder = currentNumber;
+      currentNumber += increment;
+      increment = placeHolder;
+    }
+  }
+
+  return currentNumber;
 };
 
 // Do not edit below this line
